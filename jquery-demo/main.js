9e0c32d1c0a26e7ac1d9bf4c765062c7f2f5c9f9
Ventsislav Peychev <del.busque@gmail.com>
2022-12-09 22:48:36 +0200

    Add events on click

--- jquery-demo/main.js
+++ jquery-demo/main.js
@@ -1,3 +1,7 @@
-$("#btn-fade").click(function () {
-    alert('Hello jQuery !')
-})
+$("#btn").click(() => alert('Hello jQuery !'));
+
+$(document).ready(() => $(".image").click(() => alert('Image clicked !')));
+
+$("h1.heading").click(function () {
+    $(this).hide()
+});
