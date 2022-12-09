72701083e563050f9bcd053c1865c5f45c4dcf73
Ventsislav Peychev <del.busque@gmail.com>
2022-12-09 23:35:47 +0200

    Add different events

--- jquery-demo/main.js
+++ jquery-demo/main.js
@@ -5,3 +5,21 @@ $(document).ready(() => $(".image").click(() => alert('Image clicked !')));
 $("h1.heading").click(function () {
     $(this).hide()
 });
+
+$('#p1').hover(function () {
+    $(this).hide()
+});
+
+$('#p2').click(() => $('#p2').hide());
+
+$('#p3').dblclick(() => $('#p3').hide());
+
+$('#p4').mouseout(function () {
+    $(this).hide();
+});
+
+$(document).keypress((e) => {
+    if (e.which == 13) {
+        $('#p5').hide()
+    }
+});
